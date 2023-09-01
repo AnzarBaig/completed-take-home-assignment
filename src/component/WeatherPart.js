df85f2da54e54704fa5e99cd1952bae9a19b3b41
Mirza Anzar Baig <mabaig222@gmail.com>
2023-09-01 12:57:38 +0530

    added readme

--- src/component/WeatherPart.js
+++ src/component/WeatherPart.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Image } from '@mantine/core';
-import {FaTemperatureEmpty} from "react-icons/fa6"
-import {BiSolidDropletHalf} from "react-icons/bi"
+import { FaTemperatureEmpty } from "react-icons/fa6";
+import { BiSolidDropletHalf } from "react-icons/bi";
+
 const WeatherPart = ({ weatherData, weatherIcon }) => {
   return (
     <section className="mt-6">
-      <Image maw={150} mx="auto" radius="md" src={`/icons/${weatherIcon}.svg`} alt="Weather Icon" />
+      <Image maxW="150px" mx="auto" radius="md" src={`/icons/${weatherIcon}.svg`} alt="Weather Icon" />
       <div className="text-center">
         <span className="text-7xl font-bold">
           {Math.round(weatherData.main.temp)}
@@ -21,35 +22,33 @@ const WeatherPart = ({ weatherData, weatherIcon }) => {
         </span>
       </div>
       <div className="mt-6 mb-2 flex justify-evenly border-t border-gray-300">
-      <div className="column flex items-center">
-      <div className="text-4xl text-blue-400">
-      <FaTemperatureEmpty/>
-      </div>
+        <div className="column flex items-center">
+          <div className="text-4xl text-blue-400">
+            <FaTemperatureEmpty />
+          </div>
           <div>
             <div>
+              <span className="text-lg font-semibold">
+                {Math.round(weatherData.main.feels_like)}
+              </span>
+              <span className="text-sm">°C</span>
+            </div>
+            <p>Feels like</p>
+          </div>
+        </div>
+        <div className="border-l border-gray-300"></div>
+        <div className="column flex items-center pl-2">
+          <div className="text-4xl text-blue-400">
+            <BiSolidDropletHalf />
+          </div>
+          <div className="">
             <span className="text-lg font-semibold">
-              {Math.round(weatherData.main.feels_like)}
+              {weatherData.main.humidity}%
             </span>
-            <span className="text-sm">°C</span>
+            <p>Humidity</p>
           </div>
-          <p>Feels like</p>
-          </div>
- 
-      </div>
-      <div className="border-l border-gray-300"></div>
-      
-      <div className="column flex items-center pl-2">
-      <div className="text-4xl text-blue-400">
-        <BiSolidDropletHalf/>
-        </div>
-        <div className="">
-          <span className="text-lg font-semibold">
-            {weatherData.main.humidity}%
-          </span>
-          <p>Humidity</p>
         </div>
       </div>
-    </div>
     </section>
   );
 };
